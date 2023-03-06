09ec21c764d669b2b16161749820ec645ea3cd0d
Ruhul Amin Parvez <parvez.techforing@gmail.com>
2023-03-06 18:09:45 +0600

    Initialize and Folder Structure Done :octocat:

---
+++ README.md
@@ -0,0 +1,2 @@
+## Task RepliQ - Admin Panel
+E-commerce admin panel to be built using React.js or Next.js and TailwindCSS
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './Router/Routes/Routes';
+
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
