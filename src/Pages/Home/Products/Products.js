95ee1a2f0adf60fff038caa50d646d09f84e9a51
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2023-03-09 04:11:09 +0600

    font changed :octocat:

--- src/Pages/Home/Products/Products.js
+++ src/Pages/Home/Products/Products.js
@@ -5,7 +5,7 @@ import ProductData from './ProductData';
 const Products = () => {
   return (
     <div  className='lg:px-16 bg-base-200'>
-      <div className='flex justify-center items-center mt-10'>
+      <div className='flex justify-center items-center pt-10'>
         <h2 className='text-3xl font-bold uppercase'>Products</h2>
       </div>
       <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 py-16'>
