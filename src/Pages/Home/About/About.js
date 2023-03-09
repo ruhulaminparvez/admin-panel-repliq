619fd833e2bdd54cc5d01e91a393d2cd4b3ccc30
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2023-03-09 11:31:08 +0600

    Contact and About Page Done :octocat:

--- src/Pages/Home/About/About.js
+++ src/Pages/Home/About/About.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import aboutImg from './../../../Assets/contact-about/about.svg';
 
 const About = () => {
   return (
     <div className='lg:px-16 bg-base-200'>
-      About
+      <div className='flex justify-center items-center pt-10'>
+        <h2 className='text-3xl font-bold uppercase'>About Us</h2>
+      </div>
+      <div className='flex justify-center items-center'>
+        <img src={aboutImg} alt="about" className='w-96 h-96' />
+      </div>
     </div>
   );
 };
--- src/Pages/Home/Contact/Contact.js
+++ src/Pages/Home/Contact/Contact.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import contactImg from './../../../Assets/contact-about/contact.svg';
 
 const Contact = () => {
   return (
     <div className='lg:px-16 bg-base-200'>
-      Contact
+      <div className='flex justify-center items-center pt-10'>
+        <h2 className='text-3xl font-bold uppercase'>Contact Us</h2>
+      </div>
+      <div className='flex justify-center items-center'>
+        <img src={contactImg} alt="contact" className='w-96 h-96' />
+      </div>
     </div>
   );
 };
